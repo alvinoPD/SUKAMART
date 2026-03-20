0ef0b0026d5b130a5a827eeb0923f0bf06c559bd
alvinoPD <alvinoputradika@gmail.com>
2026-03-20 21:22:49 +0700

    update table pesanan

--- crud/dataPesanan.php
+++ crud/dataPesanan.php
@@ -1,4 +1,49 @@
-<?php
+ <?php
 include "../database/config.php";
 
+$query = mysqli_query($db,"
+    SELECT id, id_pembeli, subtotal AS total, 'ganda' AS tipe
+    FROM pesanan_ganda
+    UNION ALL
+    SELECT id, id_pembeli, jumlah_total AS total, 'tunggal' AS tipe
+    FROM pesanan_tunggal
+");
+
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</head>
+<body>
+    <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">produk</th>
+      <th scope="col">jumlah</th>
+      <th scope="col">harga</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php
+    while($data = mysqli_fetch_assoc($query)){
+?>
+<tr>
+  <td><?= $data['id']; ?></td>
+  <td><?= $data['id_pembeli']; ?></td>
+  <td><?= $data['total']; ?></td>
+  <td><?= $data['tipe']; ?></td>
+</tr>
+<?php } ?>
+  
+  </tbody>
+</table>
+</body>
+</html> 
