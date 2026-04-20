da212ab529f6cbc376ee796fe01dc9ac3884c27c
alvinoPD <alvinoputradika@gmail.com>
2026-04-20 20:22:32 +0700

    20-04-2026 login masih ada error jadi aku matiin validasinya dulu

--- dashboard-user/beranda-user.php
+++ dashboard-user/beranda-user.php
@@ -1,12 +1,12 @@
 <?php
 session_start();
 
-// Cek jika tidak ada session username (belum login) atau role BUKAN pembeli
-if(!isset($_SESSION['username']) || $_SESSION['role'] != 'pembeli') {
-    // Arahkan kembali ke halaman login
-    header("Location: ../login/login.php");
-    exit();
-}
+// // Cek jika tidak ada session username (belum login) atau role BUKAN pembeli
+// if(!isset($_SESSION['username']) || $_SESSION['role'] != 'pembeli') {
+//     // Arahkan kembali ke halaman login
+//     header("Location: ../login/login.php");
+//     exit();
+// }
 
 include '../database/config.php';
 include '../part/nav-user.php';
@@ -20,7 +20,7 @@ include '../part/nav-user.php';
     <title>Beranda - SUKAMART</title>
 </head>
 <body>
-    <h1>Selamat Datang di SUKAMART, <?php echo $_SESSION['username']; ?>!</h1>
+    <!-- <h1>Selamat Datang di SUKAMART, <?php echo $_SESSION['username']; ?>!</h1> -->
     
     <?php
     // include '../crud/read-user.php'; // Kita matikan dulu sampai file/database ini siap
