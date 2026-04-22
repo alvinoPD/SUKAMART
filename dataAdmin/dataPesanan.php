a621922e748716a96a813dfd3b040795faa33f80
alvinoPD <alvinoputradika@gmail.com>
2026-04-22 20:27:10 +0700

    22-04-2026 benerin tabel

--- dataAdmin/dataPesanan.php
+++ dataAdmin/dataPesanan.php
@@ -2,11 +2,9 @@
 include "../database/config.php";
 
 $query = mysqli_query($db,"
-    SELECT id, id_pembeli, subtotal AS total, 'ganda' AS tipe
-    FROM pesanan_ganda
+    SELECT id,nama_produk,kuantitas,harga FROM pesanan_tunggal AS total
     UNION ALL
-    SELECT id, id_pembeli, jumlah_total AS total, 'tunggal' AS tipe
-    FROM pesanan_tunggal
+    SELECT id,nama_produk,kuantitas,harga FROM pesanan_ganda AS total
 ");
 
 
@@ -14,7 +12,12 @@ $query = mysqli_query($db,"
 
      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 <body>
+
+  <nav id="nav">
+     <a id="back" href="../dashboard-admin/profil.php"><i class="fa-solid fa-arrow-left-long"></i></a>
+</nav>
     <table class="table">
   <thead>
     <tr>
@@ -30,9 +33,9 @@ $query = mysqli_query($db,"
 ?>
 <tr>
   <td><?= $data['id']; ?></td>
-  <td><?= $data['id_pembeli']; ?></td>
-  <td><?= $data['total']; ?></td>
-  <td><?= $data['tipe']; ?></td>
+  <td><?= $data['nama_produk']; ?></td>
+  <td><?= $data['kuantitas']; ?></td>
+  <td><?= $data['harga']; ?></td>
 </tr>
 <?php } ?>
   
